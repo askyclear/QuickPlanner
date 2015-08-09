8ad64a3ff79054ef91046859390db9ba02ec3b8a
askyclear <askyclear2@gmail.com>
2015-08-09 15:52:00 +0900

    second commit (cherry picked from commit a26a526)

--- app/src/main/java/com/example/seon/like/Quick.java
+++ app/src/main/java/com/example/seon/like/Quick.java
@@ -2,6 +2,7 @@ package com.example.seon.like;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -9,9 +10,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 @SuppressLint("ValidFragment")
 public class Quick extends Fragment {
@@ -24,7 +25,7 @@ public class Quick extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.quick, null);
+        View view = inflater.inflate(R.layout.quick, null);
         ListView quick_lv = (ListView)view.findViewById(R.id.quick_lv);
         Button quick_add_bt = (Button)view.findViewById(R.id.quick_add_bt);
         mAdapter = new QuickAdapter();
@@ -33,40 +34,15 @@ public class Quick extends Fragment {
         mAdapter.add("Title1");
         mAdapter.add("Title2");
         mAdapter.add("Title3");
-
         quick_add_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LayoutInflater li = LayoutInflater.from(mContext);
-                View promptView = li.inflate(R.layout.customalertdialog, null);
-
-                AlertDialog.Builder ab = new AlertDialog.Builder(mContext);
-                ab.setView(promptView);
-                //final EditText text = (EditText)promptView.findViewById(R.id.editTextDialogUserInput);
-
-                ab
-                    .setCancelable(true)
-                        .setTitle("Quick add")
-                        .setMessage("Please input title")
-                    .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            //mAdapter.getItemId(0) = text.getText().toString();
-                            Toast.makeText(mContext, "onClickListen", Toast.LENGTH_SHORT);
-                        }
-                    })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    });
-
-                //AlertDialog dialog = ab.create();
-                //dialog.show();
-                ab.show();
+                FragmentManager fm = getSupportFragmentManager();
+                DialogQuick  dialogFragment = new DialogQuick();
+                dialogFragment.show(fm, "fragment_dialog_test");
             }
         });
         return view;
     }
+
 }
